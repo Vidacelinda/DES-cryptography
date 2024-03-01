571c1ce9e67dfaa221ac44987877586fcd031218
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2024-02-29 20:36:50 -0500

    comment the permutation table's and there purpose

--- des.cpp
+++ des.cpp
@@ -5,9 +5,10 @@
 
 using namespace std;
 
+// source https://en.wikipedia.org/wiki/DES_supplementary_material
 class DES_permutations_and_tables{
-	// constants regarding the keys
-
+	// **  constants regarding the keys process **
+	// permutation choice 1
 	const int pc_1[56] ={  57 ,49 ,41 ,33 ,25 ,17 ,9  ,
 				1  ,58 ,50 ,42 ,34 ,26 ,18 ,
 				10 ,2  ,59 ,51 ,43 ,35 ,27 ,
@@ -17,8 +18,8 @@ class DES_permutations_and_tables{
 				14 ,6  ,61 ,53 ,45 ,37 ,29 ,
 				21 ,13 ,5  ,28 ,20 ,12 ,4 };
 
-	int num_leftShift[16] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 }; // number of bits to shift for each iteration
-
+	
+	// permutation choice 1
 	const int pc_2[48] = {  14 ,17 ,11 ,24 ,1  ,5  ,
 				3  ,28 ,15 ,6  ,21 ,10 ,
 				23 ,19 ,12 ,4  ,26 ,8  ,
@@ -28,9 +29,13 @@ class DES_permutations_and_tables{
 				44 ,49 ,39 ,56 ,34 ,53 ,
 				46 ,42 ,50 ,36 ,29 ,32 };
 
-	// constants regarding the plain text
+	// bit rotation table : number of bits to shift for each iteration
+	int num_leftShift[16] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 }; 
 
-	const int IP_t[64] = { 	58 ,50 ,42 ,34 ,26 ,18 ,10 ,2 ,  // intital permutation table
+
+	// ** constants regarding the plain text proccess **
+	// intital permutation table
+	const int IP_t[64] = { 	58 ,50 ,42 ,34 ,26 ,18 ,10 ,2 ,  
 				60 ,52 ,44 ,36 ,28 ,20 ,12 ,4 ,
 				62 ,54 ,46 ,38 ,30 ,22 ,14 ,6 ,
 				64 ,56 ,48 ,40 ,32 ,24 ,16 ,8 ,
@@ -39,7 +44,18 @@ class DES_permutations_and_tables{
 				61 ,53 ,45 ,37 ,29 ,21 ,13 ,5 ,
 				63 ,55 ,47 ,39 ,31 ,23 ,15 ,7 };
 
-	const int E_t[48] = { 	32 ,1  ,2  ,3  ,4  ,5  , // expantion table
+	// final permutation table
+	const int P_1[64] = { 	40 ,8  ,48 ,16 ,56 ,24 ,64 ,32 ,
+				39 ,7  ,47 ,15 ,55 ,23 ,63 ,31 ,
+				38 ,6  ,46 ,14 ,54 ,22 ,62 ,30 ,
+				37 ,5  ,45 ,13 ,53 ,21 ,61 ,29 ,
+				36 ,4  ,44 ,12 ,52 ,20 ,60 ,28 ,
+				35 ,3  ,43 ,11 ,51 ,19 ,59 ,27 ,
+				34 ,2  ,42 ,10 ,50 ,18 ,58 ,26 ,
+				33 ,1  ,41 ,9  ,49 ,17 ,57 ,25 };
+
+	// expantion table
+	const int E_t[48] = { 	32 ,1  ,2  ,3  ,4  ,5  , 
 				4  ,5  ,6  ,7  ,8  ,9  ,
 				8  ,9  ,10 ,11 ,12 ,13 ,
 				12 ,13 ,14 ,15 ,16 ,17 ,
@@ -47,8 +63,8 @@ class DES_permutations_and_tables{
 				20 ,21 ,22 ,23 ,24 ,25 ,
 				24 ,25 ,26 ,27 ,28 ,29 ,
 				28 ,29 ,30 ,31 ,32 ,1 };
-
-	int S[8][4][16] = {                        // S-box
+	// S-box's (8 s-boxes in order form 1-8)
+	int S[8][4][16] = {                        
 		{
 			{ 14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7 },
 			{ 0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8 },
@@ -98,7 +114,7 @@ class DES_permutations_and_tables{
 			{ 2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11 }
 		}
 	};
-
+	// permutation tabel : shuffels the bits of a 32-bit hald block
 	const int P[32] = { 	16 ,7  ,20 ,21 ,
 				29 ,12 ,28 ,17 ,
 				1  ,15 ,23 ,26 ,
@@ -108,14 +124,6 @@ class DES_permutations_and_tables{
 				19 ,13 ,30 ,6  ,
 				22 ,11 ,4  ,25 };
 
-	const int P_1[64] = { 	40 ,8  ,48 ,16 ,56 ,24 ,64 ,32 ,
-				39 ,7  ,47 ,15 ,55 ,23 ,63 ,31 ,
-				38 ,6  ,46 ,14 ,54 ,22 ,62 ,30 ,
-				37 ,5  ,45 ,13 ,53 ,21 ,61 ,29 ,
-				36 ,4  ,44 ,12 ,52 ,20 ,60 ,28 ,
-				35 ,3  ,43 ,11 ,51 ,19 ,59 ,27 ,
-				34 ,2  ,42 ,10 ,50 ,18 ,58 ,26 ,
-				33 ,1  ,41 ,9  ,49 ,17 ,57 ,25 };
 };
 
 class functions
